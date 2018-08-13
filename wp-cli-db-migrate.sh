26e01dcf7fe235c92e770d26f71ac005a2fbd165
trainoasis <6115601+trainoasis@users.noreply.github.com>
2018-08-13 15:15:01 +0200

    Update wp-cli-db-migrate.sh
    Changed the script to accept -f or -force flag. By default it now asks for confirmation before doing anything.

--- wp-cli-db-migrate.sh
+++ wp-cli-db-migrate.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
-# FROM & TO are Wordpress Root folder absolute paths 
-# (or relative if you are running the script from specific folder)
-FROM="/your/wordpress/root/path/from" # can also be Bedrock install
-TO="/your/wordpress/root/path/to"
-REPLACEFROM="oldurl.my"
-REPLACETO="newurl.my"
+# VARIABLES
+FROM="/your/wp/root/from"
+TO="/your/wp/root/to"
+REPLACEFROM="from.url.my"
+REPLACETO="to.url.my"
 
+FORCE=false
+DRYRUN="--dry-run"
+
+# CHECK FLAGS FIRST
+if [[ $* == -force ]] || [[ $* == -f ]]
+  then
+    DRYRUN=""
+    FORCE=true
+fi
+
+# ASK USER FOR CONFIRMATION WHEN NO -f or -force flag
+if [[ $FORCE == false ]]
+  then
+
+  echo "\nPlease check the below and confirm:\n"
+  echo "${FROM}" "->" "${TO}"
+  echo "------"
+  echo  "${REPLACEFROM}" "->" "${REPLACETO}"
+  echo "\nIs this correct? [1/2]"
+  select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+  done
+
+fi
+
+# MOVE TO APPROPRIATE DIRS AND EXPORT DBS
 cd "${FROM}"
 wp db export wp-cli-db-migrate.sql
 cd "${TO}"
-wp db export wp-cli-db-migrate-db_backup.sql
-wp db import "${FROM}/wp-cli-db-migrate.sql"
-wp search-replace "${REPLACEFROM}" "${REPLACETO}"
+wp db export db_backup.sql
 
-# if all is fine, clean after ourselves
+# IMPORT DB  
+wp db import "${FROM}/wp-cli-db-migrate.sql"
+
+# SEARCH-REPLACE THE NEW DATABASE
+wp search-replace "${REPLACEFROM}" "${REPLACETO}" ${DRYRUN}
+
+# IF ALL IS FINE, CLEAN AFTER OURSELVES
 cd "${FROM}"
 rm -f "${FROM}/wp-cli-db-migrate.sql"
-cd "${TO}"
-rm "${FROM}/wp-cli-db-migrate-db_backup.sql"
+#cd "${TO}"
+#rm "${FROM}/wp-cli-db-migrate-db_backup.sql"
